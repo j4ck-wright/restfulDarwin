393ab728b5b49e6197027c27262475e0c48f5959
Jack Wright <jackw221001@gmail.com>
2023-12-22 20:34:09 +0000

    feat: swagger documentation at /

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,28 @@ import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
 import cors from 'koa2-cors';
 import logger from 'koa-logger';
+import Router from 'koa-router';
+import { koaSwagger } from 'koa2-swagger-ui';
 import { config } from './config';
+import yamljs from 'yamljs';
 
+const baseRouter = new Router();
 import healthRoutes from './routes/health';
 
 const app = new Koa();
 const PORT = config.port;
+const spec = yamljs.load('src/swagger.yaml');
+
+baseRouter.get(
+  '/',
+  koaSwagger({ routePrefix: false, swaggerOptions: { spec } })
+);
 
 app.use(bodyParser());
 app.use(cors({ origin: '*' }));
 app.use(logger());
 app.use(json());
+app.use(baseRouter.routes());
 app.use(healthRoutes.routes());
 
 export const server = app
