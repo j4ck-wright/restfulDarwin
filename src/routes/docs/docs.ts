8828933227e2bfba73b38fbb4dfc715a647a98b2
Jack Wright <jackw221001@gmail.com>
2023-12-23 18:53:11 +0000

    fix: swagger docs accessibile during deployment

--- src/routes/docs/docs.ts
+++ src/routes/docs/docs.ts
@@ -1,10 +1,11 @@
 import Router from 'koa-router';
 import { koaSwagger } from 'koa2-swagger-ui';
 import yamljs from 'yamljs';
+import path from 'path';
 
 export const router = new Router();
 
-const spec = yamljs.load('src/swagger.yaml');
+const spec = yamljs.load(path.join(__dirname, '../../swagger.yaml'));
 
 router.get(
   '/docs',
